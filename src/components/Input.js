b4f3ca7ab3099293cdad53601400bbf73c669a6a
Raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-03-27 13:11:27 -0300

    Sample components

---
+++ src/components/Input.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {SafeAreaView, StyleSheet, TextInput} from 'react-native'
+
+const Input = () =>{
+    const [text, onChangeText] = React.useState('Texto inutil');
+    const [number, onChangeNumber] = React.useState('');
+
+    <SafeAreaView>
+        <TextInput
+        value={text}
+        onChangeText={onChangeText}
+        />
+        <TextInput
+        value={number}
+        onChangeText={onChangeNumber}
+        keyboardType="numeric"/>
+    </SafeAreaView>
+}
+
+export default Input;
---
+++ src/components/ViewWithText.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ViewWithText = () =>{
+    return (
+        <View>
+            <Text>Teste - tema ?</Text>
+        </View>
+    )
+}
